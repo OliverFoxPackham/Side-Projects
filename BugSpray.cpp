a2f1bf3aab0659514ab31f45baeb780811ebdb7b
OliverFoxPackham <69836083+OliverFoxPackham@users.noreply.github.com>
2024-02-14 14:29:10 -0800

    Update BugSpray.cpp
    adding hard mode

--- BugSpray.cpp
+++ BugSpray.cpp
@@ -9,7 +9,9 @@ int gassed = 1;
 int done = 0;
 int room = 2;
 int bugs_found = 0;
+int never_look_back = 2;
 string action;
+int last = 0;
 
 int living_room();
 int kitchen();
@@ -20,9 +22,34 @@ int entryway();
 
 
 int main(){
-	cout << "you're standing in the living room of a strange house, this is nothing new to you though\n";
+
+	cout << "what mode do you want to play on, easy or hard?\n";
+	do{
+		cin >> action;
+		if (action == "hard"){
+			cout << "" << endl;
+			cout << "you have entered never look back mode\n";
+			never_look_back = 1;
+		}
+		else if (action == "easy"){
+			cout << "" << endl;
+			cout << "you are playing on easy mode\n";
+			never_look_back = 0;
+		}
+		else{
+			cout << "" << endl;
+			cout << action << " is not a valid answer, type easy or hard\n";
+		}
+	}while(never_look_back == 2);
+
+	
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "you're standing in the living_room of a strange house, this is nothing new to you though\n";
 	cout << "you are a bug exterminator and if there are bugs, you will need to gas every room in the house.\n";
 	do{
+		if (never_look_back == 0)
+			last = 0;
 		switch (room){
 			case 0:
 				cout << "you have been poisoned, better luck in the next life" << endl;
@@ -62,8 +89,13 @@ int main(){
 int living_room(){
        if ((gassed % 2) == 0)
 		return 0;
-	cout << "you have enterd the living room" << endl << "you have " << cans << " gas canisters left" << endl;
-	cout << "actions available: entryway, kitchen, gas" << endl;
+	cout << "you have enterd the living_room" << endl << "you have " << cans << " gas canisters left" << endl;
+	cout << "actions available: "; 
+	if (last != 11)
+		cout << "entryway, ";
+	if (last != 3)
+		cout << "kitchen, ";
+	cout << "gas" << endl;
 	do{
 		cin >> action;
 		if ((action == "gas")&&((gassed % 2) != 0)){
@@ -72,10 +104,22 @@ int living_room(){
 			cout << "the room has been gassed, you need to leave, where would you like to go?" << endl;
 			cin >> action;
 		}
-		if (action == "entryway")
-			return 11;
-		else if (action == "kitchen")
-			return 3;
+		if (action == "entryway"){
+			if ((last == 11)&&(never_look_back == 1))
+				cout << "you were just there, and you never look back" << endl;
+			else{
+				last = room;
+				return 11;
+			}
+		}
+		else if (action == "kitchen"){
+			if ((last == 3)&&(never_look_back == 1))
+				cout << "you were just there, and you never look back" << endl;
+			else{
+			       	last = room;
+				return 3;
+			}
+		}
 		else
 			cout << "there is no doorway to the " << action << endl;
 	}while(1==1);
@@ -90,7 +134,14 @@ int kitchen(){
 		cout << "you see the stove was left on. you turn it off. it was lucky you were there" << endl;
 		stove_on = 0;
 	}
-	cout << "actions available: entryway, living_room, bathroom, gas" << endl;
+	cout << "actions available: "; 
+	if (last != 11)
+		cout << "entryway, ";
+	if (last != 5)
+		cout << "bathroom, ";
+	if (last != 2)
+		cout << "living_room, ";
+	cout << "gas" << endl;
 	do{
  	cin >> action;
 	if ((action == "gas")&&((gassed % 3) != 0)){
@@ -99,12 +150,30 @@ int kitchen(){
 		cout << "the room has been gassed, you need to leave, where would you like to go?" << endl;
 		cin >> action;
 		}
-	if (action == "entryway")
-		return 11;
-	else if (action == "living_room")
-		return 2;
-	else if (action == "bathroom")
-		return 5;
+	if (action == "entryway"){
+		if ((last == 11)&&(never_look_back == 1))
+			cout << "you were just there, and you never look back" << endl;
+		else{
+			last = room;
+			return 11;
+		}
+	}
+	else if (action == "living_room"){
+		if ((last == 2)&&(never_look_back == 1))
+			cout << "you were just there, and you never look back" << endl;
+		else{
+			last = room;
+			return 2;
+		}
+	}
+	else if (action == "bathroom"){
+		if ((last == 5)&&(never_look_back == 1))
+			cout << "you were just there, and you never look back" << endl;
+		else{
+			last = room;
+			return 5;
+		}
+	}
 	else
 		cout << "there is no doorway to the " << action << endl;
 	}while(1==1);
@@ -119,7 +188,12 @@ int bathroom(){
 		cout << "you hear a scuttling sound in the walls. bugs. you will need to clear the house of them" << endl;
 		bugs_found = 1;
 	}
-	cout << "actions available: bedroom, kitchen, gas" << endl;
+	cout << "actions available: "; 
+	if (last != 7)
+		cout << "bedroom, ";
+	if (last != 3)
+		cout << "kitchen, ";
+	cout << "gas" << endl;
 	do{
 		cin >> action;
 		if ((action == "gas")&&((gassed % 5) != 0)){
@@ -128,10 +202,22 @@ int bathroom(){
 			cout << "the room has been gassed, you need to leave, where would you like to go?" << endl;
 			cin >> action;
 			}
-		if (action == "bedroom")
-			return 7;
-		else if (action == "kitchen")
-			return 3;
+		if (action == "bedroom"){
+			if ((last == 7)&&(never_look_back == 1))
+				cout << "you were just there, and you never look back" << endl;
+			else{
+				last = room;
+				return 7;
+			}
+		}
+		else if (action == "kitchen"){
+			if ((last == 3)&&(never_look_back == 1))
+				cout << "you were just there, and you never look back" << endl;
+			else{
+				last = room;
+				return 3;
+			}
+		}
 		else
 			cout << "there is no doorway to the " << action << endl;
 	}while(1==1);
@@ -142,7 +228,12 @@ int bedroom(){
        if ((gassed % 7) == 0)
 		return 0;
 	cout << "you have enterd the bedroom" << endl << "you have " << cans << " gas canisters left" << endl;
-	cout << "actions available: entryway, bathroom, gas" << endl;
+	cout << "actions available: "; 
+	if (last != 11)
+		cout << "entryway, ";
+	if (last != 5)
+		cout << "bathroom, ";
+	cout << "gas" << endl;
 	do{
  	cin >> action;
 	if ((action == "gas")&&((gassed % 7) != 0)){
@@ -151,10 +242,22 @@ int bedroom(){
 		cout << "the room has been gassed, you need to leave, where would you like to go?" << endl;
 		cin >> action;
 		}
-	if (action == "entryway")
-		return 11;
-	else if (action == "bathroom")
-		return 5;
+	if (action == "entryway"){
+		if ((last == 11)&&(never_look_back == 1))
+			cout << "you were just there, and you never look back" << endl;
+		else{
+			last = room;
+			return 11;
+		}
+	}
+	else if (action == "bathroom"){
+		if ((last == 5)&&(never_look_back == 1))
+			cout << "you were just there, and you never look back" << endl;
+		else{
+			last = room;
+			return 5;
+		}
+	}
 	else
 		cout << "there is no doorway to the " << action << endl;
 	}while(1==1);
@@ -165,7 +268,14 @@ int entryway(){
        if ((gassed % 11) == 0)
 		return 0;
 	cout << "you have enterd the entryway" << endl << "you have " << cans << " gas canisters left" << endl;
-	cout << "actions available: living_room, bedroom, exit, kitchen, gas" << endl;
+	cout << "actions available: "; 
+	if (last != 7)
+		cout << "bedroom, ";
+	if (last != 3)
+		cout << "kitchen, ";
+	if (last != 2)
+		cout << "living_room, ";
+	cout << "exit, gas" << endl;
 	do{
 		cin >> action;
 		if ((action == "gas")&&((gassed % 11) != 0)){
@@ -174,12 +284,30 @@ int entryway(){
 			cout << "the room has been gassed, you need to leave, where would you like to go?" << endl;
 			cin >> action;
 			}
-		if (action == "bedroom")
-			return 7;
-		else if (action == "living_room")
-			return 2;
-		else if (action == "kitchen")
-			return 3;
+		if (action == "bedroom"){
+			if ((last == 7)&&(never_look_back == 1))
+				cout << "you were just there, and you never look back" << endl;
+			else{
+				last = room;
+				return 7;
+			}
+		}
+		else if (action == "living_room"){
+			if ((last == 2)&&(never_look_back == 1))
+				cout << "you were just there, and you never look back" << endl;
+			else{
+				last = room;
+				return 2;
+			}
+		}
+		else if (action == "kitchen"){
+			if ((last == 3)&&(never_look_back == 1))
+				cout << "you were just there, and you never look back" << endl;
+			else{
+				last = room;
+				return 3;
+			}
+		}
 		else if (action == "exit"){
 			if (cans == 0)
 				return 13;
